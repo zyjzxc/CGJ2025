1c01df48f5b7ae78e8dfcf2cea795bfb993f3b5d
yhyzzz <756832012@qq.com>
2025-06-27 21:19:49 +0800

    mod: init repo

--- EIA/Assets/Scripts/Others/Bullet.cs
+++ EIA/Assets/Scripts/Others/Bullet.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private Transform m_Transfrom;
+    
+    public float Speed;
+    public Vector3 Direction;
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
